adf5aada00e99de0602d7b1afcf41732f2f5e0fa
AutumnSound <qjm1412@163.com>
2022-05-14 17:43:10 +0800

    修复获取星期几出错

--- src/main/java/per/autumn/mirai/autoreply/Utils.kt
+++ src/main/java/per/autumn/mirai/autoreply/Utils.kt
@@ -16,7 +16,7 @@ fun today(): String {
 }
 
 fun currentDayOfWeek(): String {
-    val week = arrayOf("星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日")
+    val week = arrayOf("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")
     return week[DateUtil.thisDayOfWeek() - 1]
 }
 
